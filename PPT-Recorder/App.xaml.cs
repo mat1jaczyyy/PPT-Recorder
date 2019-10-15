380a37ed7c57ad0b80e3ebd7c9c56de51a07125b
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-10-15 15:32:06 +0200

    Create App

---
+++ PPT-Recorder/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Threading;
+using System.Windows;
+
+namespace Recorder {
+    public partial class App {
+        [Conditional("DEBUG")]
+        void OverrideLocale(string locale) =>
+            Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = new CultureInfo(locale);
+
+        void Main(object sender, StartupEventArgs e) => OverrideLocale("en-US");
+
+        void Exiting(object sender, ExitEventArgs e) => Bot.Dispose();
+    }
+}
