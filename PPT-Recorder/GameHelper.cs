7572d481e3049dcfb2ef1932e522421036bec656
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-10-16 14:48:34 +0200

    Create GameHelper

---
+++ PPT-Recorder/GameHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Recorder {
+    static class GameHelper {
+        static ProcessMemory Game = new ProcessMemory("puyopuyotetris", false);
+
+        public static bool CheckProcess() => Game.CheckProcess();
+
+        public static bool TrustProcess {
+            get => Game.TrustProcess;
+            set {
+                Game.TrustProcess = value;
+            }
+        }
+
+        public static bool InSwap() {
+            if (Game.ReadBoolean(new IntPtr(0x14059894C))) {
+                if (Game.ReadBoolean(new IntPtr(0x1404385C4))) {
+                    return Game.ReadByte(new IntPtr(0x140438584)) == 3;
+                } else {
+                    return Game.ReadByte(new IntPtr(0x140573794)) == 2;
+                }
+            } else {
+                return (Game.ReadByte(new IntPtr(0x140451C50)) & 0b11101111) == 4;
+            }
+        }
+
+        public static int getBigFrameCount() {
+            int addr;
+
+            if (InSwap()) {
+                addr = Game.ReadInt32(new IntPtr(
+                    Game.ReadInt32(new IntPtr(
+                        Game.ReadInt32(new IntPtr(
+                            0x140598A20
+                        )) + 0x20
+                    )) + 0x40
+                )) + 0xF8;
+            } else {
+                addr = Game.ReadInt32(new IntPtr(
+                    Game.ReadInt32(new IntPtr(
+                        Game.ReadInt32(new IntPtr(
+                            Game.ReadInt32(new IntPtr(
+                                0x140598A20
+                            )) + 0x138
+                        )) + 0x18
+                    )) + 0x100
+                )) + 0x58;
+            }
+
+            int x = Game.ReadInt32(new IntPtr(
+                addr
+            ));
+
+            if (x == 8) {
+                return Game.ReadInt32(new IntPtr(
+                    addr + 0x8
+                ));
+            }
+
+            return x;
+        }
+
+        public static int getMenuFrameCount() => Game.ReadInt32(new IntPtr(
+            0x140461B7C
+        ));
+    }
+}
