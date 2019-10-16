e9d41d7272011015942eefd6d5c2ffc6614d332b
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-10-16 14:48:44 +0200

    Create Binary and Preferences

---
+++ PPT-Recorder/Binary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Recorder {
+    public static class Binary {
+        static readonly int Version = 0;
+
+        static byte[] CreateHeader() => Encoding.ASCII.GetBytes("PTRC").Concat(BitConverter.GetBytes(Version)).ToArray();
+
+        static int DecodeHeader(BinaryReader reader) {
+            if (!reader.ReadChars(4).SequenceEqual(new char[] { 'P', 'T', 'R', 'C' })) throw new InvalidDataException();
+            int version = reader.ReadInt32();
+
+            if (version > Version) throw new InvalidDataException();
+
+            return version;
+        }
+
+        public static MemoryStream EncodePreferences() {
+            MemoryStream output = new MemoryStream();
+
+            using (BinaryWriter writer = new BinaryWriter(output)) {
+                writer.Write(CreateHeader());
+
+                writer.Write(Preferences.PlayerInfo);
+                writer.Write(Preferences.ReplaySelect);
+                writer.Write(Preferences.EndingMenu);
+            }
+
+            return output;
+        }
+
+        public static void DecodePreferences(FileStream input) {
+            using (BinaryReader reader = new BinaryReader(input)) {
+                int version = DecodeHeader(reader);
+
+                Preferences.PlayerInfo = reader.ReadBoolean();
+                Preferences.ReplaySelect = reader.ReadBoolean();
+                Preferences.EndingMenu = reader.ReadBoolean();
+            }
+        }
+    }
+}
---
+++ PPT-Recorder/Preferences.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace Recorder {
+    public static class Preferences {
+        static readonly string UserPath = Path.Combine(
+            Environment.GetEnvironmentVariable("USERPROFILE"),
+            ".ppt-recorder"
+        );
+
+        static readonly string FilePath = Path.Combine(UserPath, "PPT-Recorder.config");
+
+        static bool Initialized = false;
+
+        static bool _info = false;
+        public static bool PlayerInfo {
+            get => _info;
+            set {
+                _info = value;
+                Save();
+            }
+        }
+
+        static bool _select = false;
+        public static bool ReplaySelect {
+            get => _select;
+            set {
+                _select = value;
+                Save();
+            }
+        }
+
+        static bool _ending = false;
+        public static bool EndingMenu {
+            get => _ending;
+            set {
+                _ending = value;
+                Save();
+            }
+        }
+
+        public static void Save() {
+            if (!Initialized) return;
+
+            if (!Directory.Exists(UserPath)) Directory.CreateDirectory(UserPath);
+
+            try {
+                File.WriteAllBytes(FilePath, Binary.EncodePreferences().ToArray());
+            } catch (IOException) { }
+        }
+
+        static Preferences() {
+            if (File.Exists(FilePath))
+                using (FileStream file = File.Open(FilePath, FileMode.Open, FileAccess.Read))
+                    try {
+                        Binary.DecodePreferences(file);
+                    } catch { }
+
+            Initialized = true;
+        }
+    }
+}
