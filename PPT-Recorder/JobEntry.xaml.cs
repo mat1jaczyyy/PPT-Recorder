c0ce6693cc4114a5e154625b4585c7810a75d436
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-10-20 17:47:32 +0200

    Implement JobEntry

---
+++ PPT-Recorder/JobEntry.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Recorder {
+    public partial class JobEntry: TextBox {
+        bool _valid = false;
+        public bool IsValid {
+            get => _valid;
+            private set => Foreground = new SolidColorBrush(
+                (_valid = value)? Color.FromRgb(220, 220, 220) : Color.FromRgb(220, 0, 0)
+            );
+        }
+
+        public Job Job { get; private set; }
+
+        public JobEntry() {
+            InitializeComponent();
+        }
+
+        public void Revalidate() {
+            if (IsValid = Job.TryParse(Text, out Job job))
+                Job = job;
+        }
+
+        void InputChanged(object sender, TextChangedEventArgs e) {
+            Revalidate();
+
+            if (UI.Window == null) return;
+
+            UI.Window.RefreshJobs();
+        }
+    }
+}
